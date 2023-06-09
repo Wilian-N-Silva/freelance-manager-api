f572e4a2a1bc5a0a046626d83cbc74db8dc5be5a
Wilian-N-Silva <wilian.silva209@gmail.com>
2023-06-09 00:32:04 -0300

    fix: :bug: Fix a missing field on client model

--- src/routes/client.ts
+++ src/routes/client.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import { z } from "zod"
+import { log } from "console";
 
 export async function clientRoutes(app: FastifyInstance) {
   app.addHook('preHandler', async (request) => {
@@ -28,9 +29,10 @@ export async function clientRoutes(app: FastifyInstance) {
     return clientList
   })
 
-  app.post('/client/register', async (request) => {
+  app.post('/client', async (request) => {
     const bodySchema = z.object({
       name: z.string(),
+      isCompany: z.coerce.boolean().default(true),
       taxPayerRegistry: z.string().optional(),
       registrationNumber: z.string().optional(),
       companyName: z.string().optional(),
@@ -45,6 +47,7 @@ export async function clientRoutes(app: FastifyInstance) {
 
     const {
       name,
+      isCompany,
       taxPayerRegistry,
       registrationNumber,
       companyName,
@@ -60,7 +63,8 @@ export async function clientRoutes(app: FastifyInstance) {
     let client = await prisma.client.create({
       data: {
         name: name,
-        taxPayerRegistry: taxPayerRegistry,
+        isCompany: isCompany,
+        taxPayerRegistry: !!taxPayerRegistry ? taxPayerRegistry : null,
         registrationNumber: registrationNumber,
         companyName: companyName,
         tradingName: tradingName,
@@ -68,7 +72,6 @@ export async function clientRoutes(app: FastifyInstance) {
         email: email,
       }
     })
-    
 
     if (zipcode) {
       let adress = await prisma.address.create({
@@ -90,6 +93,90 @@ export async function clientRoutes(app: FastifyInstance) {
               id: adress.id
             }
           }
+        }
+      })
+    }
+
+    return client
+  })
+
+  app.put('/client/:id', async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const bodySchema = z.object({
+      name: z.string(),
+      isCompany: z.coerce.boolean().default(true).optional(),
+      taxPayerRegistry: z.string().optional(),
+      registrationNumber: z.string().optional(),
+      companyName: z.string().optional(),
+      tradingName: z.string().optional(),
+      phone: z.string().optional(),
+      email: z.string().optional(),
+      zipcode: z.string().optional(),
+      street: z.string().optional(),
+      number: z.string().optional(),
+      district: z.string().optional()
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const {
+      name,
+      isCompany,
+      taxPayerRegistry,
+      registrationNumber,
+      companyName,
+      tradingName,
+      phone,
+      email,
+      zipcode,
+      street,
+      number,
+      district
+    } = bodySchema.parse(request.body)
+
+    let client = await prisma.client.findUniqueOrThrow({
+      where: {
+        id: id,
+      }
+    })
+
+    // if (client.representativeId !== request.user.sub) {
+    //   return reply
+    //     .code(401)
+    //     .header('Content-Type', 'application/json; charset=utf-8')
+    //     .send({ message: '409 - Unauthorized', reason: 'Invalid Credentials' })
+    // }
+
+
+    client = await prisma.client.update({
+      where: {
+        id: id,
+      },
+      data: {
+        name: name,
+        isCompany: isCompany,
+        taxPayerRegistry: taxPayerRegistry,
+        registrationNumber: registrationNumber,
+        companyName: companyName,
+        tradingName: tradingName,
+        phone: phone,
+        email: email,
+      }
+    })
+
+
+    if (zipcode) {
+      let adress = await prisma.address.update({
+        where: {
+          id: client.addressId!
+        },
+        data: {
+          zipcode: zipcode,
+          street: street!,
+          number: number!,
+          district: district!
         }
       })
     }
